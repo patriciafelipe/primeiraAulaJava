46841464f98ac790b017f794eb6f6476bf7ab4bb
PHCJ <paulo.jesus30@etec.sp.gov.br>
2018-08-12 22:38:39 -0300

    Primeira aula Java

---
+++ src/aula/Primeira.java
@@ -0,0 +1,9 @@
+package aula;
+
+public class Primeira {
+	public static void main (String args[]) {
+		//Nesse comando podemos imprimir um texto.
+		int numero = 20, numero2 = 40;
+		System.out.println(numero+numero2);
+	}
+}
