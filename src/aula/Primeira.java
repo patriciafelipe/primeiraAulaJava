013d1e66ee498b2137d011e3ac2587390b4ab041
patriciafelipe <patricia-felipe@hotmail.com>
2018-08-20 22:25:53 -0300

    Segunda aula de java

--- src/aula/Primeira.java
+++ src/aula/Primeira.java
@@ -3,7 +3,28 @@ package aula;
 public class Primeira {
 	public static void main (String args[]) {
 		//Nesse comando podemos imprimir um texto.
-		int numero = 20, numero2 = 40;
+		/*int numero = 20, numero2 = 40, total = 0;
 		System.out.println(numero+numero2);
+		/*
+
+		/*if(total > 10) {
+			System.out.println(total);
+		}else if(total == 10) {
+			System.out.println("O valor é igual a 10");
+		}else{
+			System.out.println("O valor é inferior a 10");
+		}*/
+		
+		int i = 0;
+		
+		while(i <= 10) {
+			System.out.println("TP é maravilhoso");
+			i = i + 1;
+		
+		
+			/*for(int x = 10; x > 0; x = x - 1) {
+				System.out.println(x);
+				*/
+			}
+			}
 	}
-}
